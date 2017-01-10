e4cd0c5083c573b2f4c55076fbd79ee86b6dfdff
Keil Carpenter <Keil Carpenter>
2017-01-11 10:38:24 +1300

    search for movies

--- src/app/components/movies/movies.component.ts
+++ src/app/components/movies/movies.component.ts
@@ -10,12 +10,19 @@ export class MoviesComponent implements OnInit {
 
   popularList: Array<Object>;
   theatresList: Array<Object>;
+  searchString: string;
+  searchResults: Array<Object>;
 
+  /* Subscribe to movie service */
   constructor(private _movieService: MovieService) {
     this._movieService.GetPopularMovies().subscribe(res => {this.popularList = res.results;});
     this._movieService.GetMoviesInTheaters().subscribe(res => {this.theatresList = res.results;});
   }
 
+  /* Pass query string to search */
+  SearchMovies(){
+    this._movieService.SearchMovies(this.searchString).subscribe(res => {this.searchResults = res.results;})
+  }
   ngOnInit() {
   }
 
--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -24,4 +24,10 @@ export class MovieService {
     return this._jsonp.get('https://api.themoviedb.org/3/discover/movie?callback=JSONP_CALLBACK&primary_release_date.gte=2017-01-11&primary_release_date.lte=2017-02-28&api_key='+this.apiKey)
       .map(res => res.json());
   }
+
+  /* Search for movies */
+  SearchMovies(search: string){
+    return this._jsonp.get('https://api.themoviedb.org/3/search/movie?callback=JSONP_CALLBACK&query='+search+'&sort_by=popularity.desc&api_key='+this.apiKey)
+      .map(res => res.json());
+  }
 }
