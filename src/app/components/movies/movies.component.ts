fa6ba62bf7b5a8e47c0e7849609067f2f722a776
Keil Carpenter <Keil Carpenter>
2017-01-11 09:45:55 +1300

    get movies via api

--- src/app/components/movies/movies.component.ts
+++ src/app/components/movies/movies.component.ts
@@ -8,7 +8,10 @@ import {MovieService} from "../../services/movie.service";
 })
 export class MoviesComponent implements OnInit {
 
-  constructor(private _movieService: MovieService) { }
+  constructor(private _movieService: MovieService) {
+    this._movieService.GetPopularMovies().subscribe(res => {console.log(res.results)});
+    this._movieService.GetMoviesInTheaters().subscribe(res => {console.log(res.results)});
+  }
 
   ngOnInit() {
   }
--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -1,12 +1,27 @@
 import { Injectable } from '@angular/core';
 import {Jsonp} from "@angular/http";
+import 'rxjs/Rx';
+
 
 @Injectable()
 export class MovieService {
   apiKey: string;
+
+  /* Setup api */
   constructor(private _jsonp: Jsonp) {
     this.apiKey = '94a26a4e3ae5d82a3c426e5bec3150e5';
     console.log('MovieService Initilalized');
   }
 
+  /* Get popular movies*/
+  GetPopularMovies(){
+    return this._jsonp.get('https://api.themoviedb.org/3/discover/movie?callback=JSONP_CALLBACK&sort_by=popularity.desc&api_key='+this.apiKey)
+      .map(res => res.json());
+  }
+
+  /* Get movies in theaters */
+  GetMoviesInTheaters (){
+    return this._jsonp.get('https://api.themoviedb.org/3/discover/movie?callback=JSONP_CALLBACK&primary_release_date.gte=2017-01-11&primary_release_date.lte=2017-02-28&api_key='+this.apiKey)
+      .map(res => res.json());
+  }
 }
