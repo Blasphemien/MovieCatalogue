9c3499a5776a15cb9f3e8f6d7cabd992c0cae6c1
Keil Carpenter <Keil Carpenter>
2017-01-11 10:09:51 +1300

    show popular and 'In Theaters' movies

--- src/app/components/movies/movies.component.ts
+++ src/app/components/movies/movies.component.ts
@@ -8,9 +8,12 @@ import {MovieService} from "../../services/movie.service";
 })
 export class MoviesComponent implements OnInit {
 
+  popularList: Array<Object>;
+  theatresList: Array<Object>;
+
   constructor(private _movieService: MovieService) {
-    this._movieService.GetPopularMovies().subscribe(res => {console.log(res.results)});
-    this._movieService.GetMoviesInTheaters().subscribe(res => {console.log(res.results)});
+    this._movieService.GetPopularMovies().subscribe(res => {this.popularList = res.results;});
+    this._movieService.GetMoviesInTheaters().subscribe(res => {this.theatresList = res.results;});
   }
 
   ngOnInit() {
