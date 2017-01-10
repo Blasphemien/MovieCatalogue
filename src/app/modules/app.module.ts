782c21cdcc873ace3672e5eb6381c8a942c6e0c4
Keil Carpenter <Keil Carpenter>
2017-01-11 08:22:56 +1300

    change folder structure

--- src/app/modules/app.module.ts
+++ src/app/modules/app.module.ts
@@ -3,8 +3,8 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
-import { AppComponent } from './app.component';
-import { MoviesComponent } from './movies/movies.component';
+import { AppComponent } from '../components/app/app.component';
+import { MoviesComponent } from '../components/movies/movies.component';
 
 @NgModule({
   schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
