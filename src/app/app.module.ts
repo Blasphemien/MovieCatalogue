63471e0083fb6e7a8c7a603c1e3516d58fdfb4bb
Keil Carpenter <Keil Carpenter>
2017-01-10 17:57:16 +1300

    setup movies component with static dummy data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { MoviesComponent } from './movies/movies.component';
 
 @NgModule({
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
   declarations: [
-    AppComponent
+    AppComponent,
+    MoviesComponent
   ],
   imports: [
     BrowserModule,
